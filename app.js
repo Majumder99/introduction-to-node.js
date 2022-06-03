132fff95e80b693c736de221db72f4cb5cfb717c
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-06-03 12:37:43 +0600

    new commit of express

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ var express = require("express");
 
 var app = express();
 
-app.get();
+app.get("/", (req, res) => {
+  res.send("This is homepage");
+});
+
+app.listen(3000, (err) => {
+  if (err) throw err;
+  console.log("Server is running");
+});
