b66f6c4de3c48b505a23339b6bff8c4a759d0131
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-06-03 13:11:52 +0600

    ejs file

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var express = require("express");
-
 var app = express();
+
+app.set("view engine", "ejs"); //View engine means how browser will see it
 
 app.get("/", (req, res) => {
   res.send("This is homepage");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("This is contact page");
+});
+
+app.get("/profile/:id", (req, res) => {
+  //   res.send("You are about to see the id " + req.params.id);
+  var data = {
+    name: "sourav",
+    age: 25,
+  };
+  res.render("profile", { person: req.params.id, data: data });
 });
 
 app.listen(3000, (err) => {
